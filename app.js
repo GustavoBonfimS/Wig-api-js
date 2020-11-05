7df97c7c87dba7ef643da7345ca6da53dc50a1ce
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2020-11-05 19:05:58 -0300

    refact empress use cors, add db tables structures on txt

--- app.js
+++ app.js
@@ -7,21 +7,7 @@ const cors = require('cors');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Header',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).send({});
-    }
-    app.use(cors());
-
-    next();
-});
+app.use(cors());
 
 //routes
 const rotaUsuarios = require('./routes/usuarios');
